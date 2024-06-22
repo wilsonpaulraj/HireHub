9704598a4514b6f51f5dd1ed373875636d7b9333
Wilsonpaulraj <wilsonpaulrajd@gmail.com>
2024-06-22 23:54:38 +0530

    commit recent changes

--- src/Pages/SearchJobs.jsx
+++ src/Pages/SearchJobs.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
 import JobSearchBanner from '../Components/JobSearchBanner'
+<<<<<<< HEAD
 import FilterSection from '../Components/FilterSection'
 import JobResults from '../Components/JobResults'
+=======
+>>>>>>> a872668bacae87a354f6171aa48d0546118bb0b2
 
 const SearchJobs = () => {
 
   return (
+<<<<<<< HEAD
     <div className=' my-32 mx-56'>
       <JobSearchBanner />
       <div className='flex w-full'>
         <FilterSection />
         <JobResults />
       </div>
+=======
+    <div className=' my-36 mx-2'>
+      <JobSearchBanner />
+>>>>>>> a872668bacae87a354f6171aa48d0546118bb0b2
     </div>
   )
 }
